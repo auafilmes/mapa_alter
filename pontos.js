dfdae6242277a7be4b79fcf370cd8b74b1580795
auafilmes <rafael.auafilmes@gmail.com>
2025-06-13 00:22:37 -0300

    Add files via upload

---
+++ pontos.js
@@ -0,0 +1,23 @@
+[
+  {
+    "nome": "Restaurante Peixaria do Gordo",
+    "tipo": "restaurante",
+    "latitude": -2.504223,
+    "longitude": -54.951298,
+    "descricao": "Peixes regionais e pratos típicos amazônicos."
+  },
+  {
+    "nome": "Supermercado Alter",
+    "tipo": "supermercado",
+    "latitude": -2.505888,
+    "longitude": -54.949776,
+    "descricao": "Produtos locais, frutas e itens básicos."
+  },
+  {
+    "nome": "Feira de Artesanato Central",
+    "tipo": "feira",
+    "latitude": -2.503900,
+    "longitude": -54.951000,
+    "descricao": "Artesanato regional e lembranças de Alter do Chão."
+  }
+]
---
+++ script.js
@@ -0,0 +1,50 @@
+mapboxgl.accessToken = 'SUA_CHAVE_MAPBOX_AQUI';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-54.9495, -2.5045], // Centro Alter do Chão
+  zoom: 15
+});
+
+let userMarker;
+navigator.geolocation.getCurrentPosition((position) => {
+  const userCoords = [position.coords.longitude, position.coords.latitude];
+  userMarker = new mapboxgl.Marker({ color: 'blue' })
+    .setLngLat(userCoords)
+    .addTo(map);
+}, () => alert("Não foi possível acessar sua localização."));
+
+function traçarRota(destino) {
+  navigator.geolocation.getCurrentPosition((position) => {
+    const origem = [position.coords.longitude, position.coords.latitude];
+    fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${origem[0]},${origem[1]};${destino[0]},${destino[1]}?geometries=geojson&access_token=${mapboxgl.accessToken}`)
+      .then(res => res.json())
+      .then(data => {
+        const rota = data.routes[0].geometry;
+        map.getSource('rota')?.setData(rota) || map.addSource('rota', {
+          type: 'geojson', data: rota
+        });
+        map.getLayer('rota') || map.addLayer({
+          id: 'rota', type: 'line', source: 'rota',
+          paint: { 'line-color': '#3b9ddd', 'line-width': 5 }
+        });
+      });
+  });
+}
+
+fetch('pontos.js')
+  .then(res => res.json())
+  .then(locais => {
+    const lista = document.getElementById('local-list');
+    locais.forEach(l => {
+      const el = document.createElement('li');
+      el.textContent = `${l.nome} (${l.tipo})`;
+      el.onclick = () => {
+        new mapboxgl.Popup().setLngLat([l.longitude, l.latitude]).setHTML(`<strong>${l.nome}</strong><p>${l.descricao}</p>`).addTo(map);
+        traçarRota([l.longitude, l.latitude]);
+      };
+      lista.appendChild(el);
+      new mapboxgl.Marker().setLngLat([l.longitude, l.latitude]).addTo(map);
+    });
+  });
